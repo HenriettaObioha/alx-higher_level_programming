5df3fb49b71ee159eb39de07be8212afb2a5e04f
Henrietta Obioha <HenriettaObioha1@gmail.com>
2023-08-15 08:09:03 -0700

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all rows of table second_table
+-- from the database hbtn_0c_0
+-- results display score and name
+-- oredered by score
+SELECT score, name FROM second_table ORDER BY score DESC;
