e94c8bfb8702b07c1bc14ce66f93fb42035a4c7c
Henrietta Obioha <Henriettaobioha1@gmail.com>
2023-10-02 05:54:39 -0700

    100-github_commit.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""script lists 10 most recent commits on a agiven GitHub repo"""
+"""script lists 10 most recent commits on a agiven GitHub repo
+Usage: ./100-ithub_commits.py <repository name> <repository owner>
+"""
 
 import sys
 import requets
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[2, sys.arv[1]]
+            sys.argv[2], sys.argv[1])
 
-            r = requests.get(url)
-            commits = r.json()
-            try:
-            for i in range(10:
-                print("{}: {}".format(
-                    commits[i].get("sha"),
-                    commits[i].get("commit").get("author").get("name")))
-                except IndexError:
-                pass
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
