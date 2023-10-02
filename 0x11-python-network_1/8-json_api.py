efff9e56df09ef4fc0bad472887ab0318059670a
Henrietta Obioha <Henriettaobioha1@gmail.com>
2023-10-02 05:38:06 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """script sends request to http://0.0.0.0:5000/search_user with a given letter.
 if no letter is provided, sends 'q=""'
+- The letter is sent as the value of the variable 'q'
 """
 
 import sys
 import requests
 
 if __name__ == "__main__":
-    letter = "" 
-    if len(sys.argv) == 1 else sys.arg[1]
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
 payload = {"q": letter}
 
 r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
