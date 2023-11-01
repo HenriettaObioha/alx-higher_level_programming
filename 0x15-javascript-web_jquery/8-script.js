b2d34924c2b7b9123ea12b1d405304e6fd7cb955
Henrietta Obioha <henriettaobioha1@gmail.com>
2023-11-01 13:06:15 +0000

    jquery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,12 @@
+// JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json
+
+$(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data, textStatus) {
+    if (textStatus === 'success') {
+      let films = data.results;
+      for (let i in films) {
+        $('UL#list_movies').append('<li>' + films[i].title + '</li>');
+      }
+    }
+  });
+});
